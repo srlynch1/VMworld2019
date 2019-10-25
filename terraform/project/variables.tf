b9e0c816836214dad13f3ebf08903c912c027ccd
Simon Lynch <slynch@vmware.com>
2019-10-25 14:22:22 +1100

    update vars

--- terraform/project/variables.tf
+++ terraform/project/variables.tf
@@ -4,13 +4,13 @@ variable "url" {
 variable "refresh_token" {
 }
 ### Config for vRA Provider - Cloud Zone
-variable "cloud_account" {
-}
-variable "region_1" {
-}
+#variable "cloud_account" {
+#}
+#variable "region_1" {
+#}
 
-variable "region_2" {
-}
+#variable "region_2" {
+#}
 
 variable "zones" {
   type    = list(string)
