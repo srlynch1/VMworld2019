40afb1c5677430921c9d60e58841d94b0e5dcbe2
Simon Lynch <44986346+srlynch1@users.noreply.github.com>
2020-03-17 11:10:28 +1100

    Update main.tf

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -4,6 +4,7 @@ provider "aws" {
   region = "${var.vpc_region}"
   access_key = "${var.aws_access_key_id}"
   secret_key = "${var.aws_secret_access_key}"
+  token = "${var.aws_session_token}"
 }
 
 # Define a vpc
